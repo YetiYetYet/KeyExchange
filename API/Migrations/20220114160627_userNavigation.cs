50bb012c5a6da15a51528057ac7344b02c688114
YetiYetYet <alexisbehier22@gmail.com>
2022-01-14 20:54:12 +0100

    Migrations

---
+++ API/Migrations/20220114160627_userNavigation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class userNavigation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_application_users_roles_role_id",
+                schema: "keys_exchange",
+                table: "application_users");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "role_id",
+                schema: "keys_exchange",
+                table: "application_users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_application_users_roles_role_id",
+                schema: "keys_exchange",
+                table: "application_users",
+                column: "role_id",
+                principalSchema: "keys_exchange",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_application_users_roles_role_id",
+                schema: "keys_exchange",
+                table: "application_users");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "role_id",
+                schema: "keys_exchange",
+                table: "application_users",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_application_users_roles_role_id",
+                schema: "keys_exchange",
+                table: "application_users",
+                column: "role_id",
+                principalSchema: "keys_exchange",
+                principalTable: "roles",
+                principalColumn: "id");
+        }
+    }
+}
---
+++ API/Migrations/20220114164333_userNavigation3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class userNavigation3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_games_application_users_user_id",
+                schema: "keys_exchange",
+                table: "games");
+
+            migrationBuilder.DropIndex(
+                name: "ix_games_user_id",
+                schema: "keys_exchange",
+                table: "games");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "user_id1",
+                schema: "keys_exchange",
+                table: "games",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_games_user_id",
+                schema: "keys_exchange",
+                table: "games",
+                column: "user_id1");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_games_application_users_user_id",
+                schema: "keys_exchange",
+                table: "games",
+                column: "user_id1",
+                principalSchema: "keys_exchange",
+                principalTable: "application_users",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_games_application_users_user_id",
+                schema: "keys_exchange",
+                table: "games");
+
+            migrationBuilder.DropIndex(
+                name: "ix_games_user_id",
+                schema: "keys_exchange",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "user_id1",
+                schema: "keys_exchange",
+                table: "games");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_games_user_id",
+                schema: "keys_exchange",
+                table: "games",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_games_application_users_user_id",
+                schema: "keys_exchange",
+                table: "games",
+                column: "user_id",
+                principalSchema: "keys_exchange",
+                principalTable: "application_users",
+                principalColumn: "id");
+        }
+    }
+}
